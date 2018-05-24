4b724edb4623cf50fb6057184fc1ff5d7b71e94f
AlexLP <alexlepoupon@gmail.com>
2018-05-24 16:18:03 +0200

    product component v0.2

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="home">
+  <div class="products">
     <h1>{{ msg }}</h1>
-    <p> Total Rows : {{ totalRows }} </p>
-    <p>{{ list }}</p>
-  </div>
 
+    <b-card-group deck>
+        <div v-for="product in list" v-bind:key="product._id">
+            <b-card :title="product.name"
+                    :img-src="product.photo_url"
+                    style="max-width: 20rem;"
+                    class="h-100">
+                <p class="card-text">
+                {{product.price}} euros
+                </p>
+                <b-button variant="primary" v-on:click="addProduct(product)">Ajouter au panier</b-button>
+            </b-card>
+        </div>
+    </b-card-group>
+
+    <div class="basket">
+        <h1>Mon panier</h1>
+        <div v-for="product in basket" v-bind:key="product._id">
+            <p>{{product.name}}</p>
+            <p>{{product.quantity}}</p>
+            <p>{{product.priceBasket}}</p>
+        </div>
+        <!-- <p>{{basket}}<p> -->
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -15,17 +37,17 @@ export default {
   name: 'Product',
   data () {
     return {
-      msg: 'Welcome to Pickles App',
+      msg: 'Liste des produits',
       list: [],
-      totalRows: 0
+      totalRows: 0,
+      basket:[]
     };
   },
   beforeMount () {
     this.$http.get('http://localhost/product/list').then(response => {
-      // Fill the work's list
+      // Fill the product's list
       response.body.forEach(product => {
         this.list.push(product);
-        console.log(product);
         // this.$http.get('http://localhost/work/' + work.id).then(response => {
         //   this.list.push(response.body);
         // }, response => {
@@ -38,6 +60,19 @@ export default {
     });
   },
   methods: {
+      addProduct: function (product) {
+        //   console.log(product);
+          if(!product.quantity) {
+            product.quantity = 1;
+            product.priceBasket = product.price;
+            this.basket.push(product);
+          } else {
+            product.quantity += 1;
+            product.priceBasket += product.price;
+            product.priceBasket = Math.round(product.priceBasket * 100) / 100;
+            this.basket.push('');
+          }
+      }
   },
   components: {
   }
@@ -57,13 +92,24 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
-}
-.home {
+.products {
   padding-top: 2rem;
 }
-img {
-  max-width: 60%;
+
+.wrapper {
+  display: grid;
 }
+
+.fill {
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+}
+
+.product {
+  margin: 10px;
+}
+
+.product:hover {
+  cursor: pointer;
+}
+
 </style>
